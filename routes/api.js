3b2e780de6184e63bf4a94a419e0597dd4fa64c9
chapmanCC <chapboy1989@gmail.com>
2019-09-22 23:59:33 +0800

    finish route but not fixed

--- routes/api.js
+++ routes/api.js
@@ -1,24 +1,62 @@
 const express = require("express");
 const router = express.Router();
 
+// db
+const db = require("../db");
+const { Courses, Users } = db.Model;
+
 router
     .route("/users")
     .get((req, res) => res.status(200).json())
-    .post((req, res) => {
-        res.status(201).json();
-        // TODO - set Location header to "/", no content
-    });
+    .post((req, res) => res.status(201).redirect("/"));
 
 router
     .route("/courses")
-    .get((req, res) => res.send(/** 200 returns list of courses */))
-    .post((req, res) =>
-        res.send(/** returns nothing, creates courses, header to URI of the course*/)
-    );
+    .get((req, res) =>
+        Courses.findAll()
+            .then(courses => res.json(courses))
+            .catch(err => console.error(err))
+    ) // is
+    .post((req, res) => {
+        console.log(req.body);
+        const { title, description, estimatedTime, materialsNeeded } = req.body;
+        Courses.create({
+            title,
+            description,
+            estimatedTime,
+            materialsNeeded
+        })
+            .then(() => res.status(201).end())
+            .catch(err => console.error(err));
+    });
 
-router.route("/courses/:id");
-// .get((req, res))
-// .put((req, res))
-// .delete((req, res));
+router
+    .route("/courses/:id")
+    .get((req, res) => {
+        const { id } = req.params;
+        Courses.findByPk(id)
+            .then(course => res.json(course))
+            .catch(err => console.error(err));
+    })
+    .put((req, res) => {
+        const { id } = req.params;
+        const { title, description, estimatedTime, materialsNeeded } = req.body;
+        Courses.findByPk(id)
+            .update({
+                title,
+                description,
+                estimatedTime,
+                materialsNeeded
+            })
+            .then(() => res.status(204).end())
+            .catch(err => console.error(err));
+    })
+    .delete((req, res) => {
+        const { id } = req.params;
+        Courses.findByPk(id)
+            .destroy()
+            .then(() => res.status(204).end())
+            .catch(err => console.error(err));
+    });
 
 module.exports = router;
