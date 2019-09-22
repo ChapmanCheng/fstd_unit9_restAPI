548f03cc5a9faeebbb7fc78fd600c42e65dde9bf
chapmanCC <chapboy1989@gmail.com>
2019-09-22 22:57:45 +0800

    edit db associations

--- db/index.js
+++ db/index.js
@@ -15,4 +15,22 @@ const db = {
     Sequelize,
     Model: {}
 };
+
+const Users = require("./models/User")(sequelize);
+const Courses = require("./models/Course")(sequelize);
+
+// Associations
+Courses.belongsTo(Users, {
+    foreignKey: {
+        allowNull: true
+    }
+});
+Users.hasMany(Courses, {
+    foreignKey: {
+        allowNull: true
+    }
+});
+
+db.Model = { Users, Courses };
+
 module.exports = db;
