aed957aef44a70e280104937f8779b830a076047
Chapman Cheng <chapman@itwakes.me>
2019-09-24 13:19:10 +0800

    all api routes will next(err)

--- routes/api.js
+++ routes/api.js
@@ -10,26 +10,26 @@ const auth = require("basic-auth");
 const db = require("../db");
 const { Courses, Users } = db.Model;
 
-router.post("/users", async (req, res) => {
+router.post("/users", async (req, res, next) => {
 	const user = req.body;
 	user.password = bcrypt.hashSync(user.password.toString(), 10); //
 	// user contains "password", "firstName", "lastName", "email"
 	Users.create(user)
 		.then(() => res.status(201).end())
-		.catch(err => console.error(err));
+		.catch(err => next(err));
 });
 
-router.get("/courses", (req, res) =>
+router.get("/courses", (req, res, next) =>
 	Courses.findAll()
 		.then(courses => res.json(courses))
 		.catch(err => console.error(err))
 );
 
-router.get("/courses/:id", (req, res) => {
+router.get("/courses/:id", (req, res, next) => {
 	const { id } = req.params;
 	Courses.findByPk(id)
 		.then(course => res.json(course))
-		.catch(err => console.error(err));
+		.catch(err => next(err));
 });
 
 /**
@@ -42,29 +42,29 @@ router.get("/courses/:id", (req, res) => {
 //         res.status(401).end();
 //     }
 // });
-router.get("/users", (req, res) => res.status(200).json()); //TODO: need to return current authed user
-router.post("/courses", (req, res) => {
+router.get("/users", (req, res, next) => res.status(200).json()); //TODO: need to return current authed user
+router.post("/courses", (req, res, next) => {
 	// req.body contains "title", "description", "userId", "estimatedTime" (allowNull), "materialsNeeded"(allowNull)
 	Courses.create(req.body)
 		.then(() => res.status(201).end())
-		.catch(err => console.error(err));
+		.catch(err => next(err));
 });
 router
 	.route("/courses/:id")
-	.put((req, res) => {
+	.put((req, res, next) => {
 		const { id } = req.params;
 		// req.body contains "title", "description", "userId", "estimatedTime" (allowNull), "materialsNeeded"(allowNull)
 		Courses.findByPk(id)
 			.then(course => course.update(req.body))
 			.then(() => res.status(204).end())
-			.catch(err => console.error(err));
+			.catch(err => next(err));
 	})
-	.delete((req, res) => {
+	.delete((req, res, next) => {
 		const { id } = req.params;
 		Courses.findByPk(id)
 			.then(course => course.destroy())
 			.then(() => res.status(204).end())
-			.catch(err => console.error(err));
+			.catch(err => next(err));
 	});
 
 module.exports = router;
