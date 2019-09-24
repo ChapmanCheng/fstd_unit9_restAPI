740c490460dcedf06c499c1bfc81e7d53b936a0f
chapmanCC <chapboy1989@gmail.com>
2019-09-25 00:45:33 +0800

    fix authenticateUser else

--- expressMiddlewares/authenticateUser.js
+++ expressMiddlewares/authenticateUser.js
@@ -6,20 +6,21 @@ const db = require("../db");
 const { Users } = db.Model;
 
 const authenticateUser = async (req, res, next) => {
-	const cred = auth(req);
-	if (cred) {
-		const user = await Users.findOne({
-			where: { emailAddress: cred.name }
-		});
-		if (user) {
-			const authentication = bcrypt.compareSync(cred.pass, user.password);
-			if (authentication) {
-				req.currentUser = user;
-				next();
-			}
-		}
-	}
-	res.status(401).end();
+    const cred = auth(req);
+    if (cred) {
+        const user = await Users.findOne({
+            where: { emailAddress: cred.name }
+        });
+        if (user) {
+            const authentication = bcrypt.compareSync(cred.pass, user.password);
+            if (authentication) {
+                req.currentUser = user;
+                next();
+            }
+        }
+    } else {
+        res.status(401).end();
+    }
 };
 
 module.exports = authenticateUser;
