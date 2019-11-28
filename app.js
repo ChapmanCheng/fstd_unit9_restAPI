de283d852028d3a360253c249ea875c8fa08b53d
chapmanCC <chapboy1989@gmail.com>
2019-11-28 23:14:47 +0800

    centralize app.js erros

--- app.js
+++ app.js
@@ -39,10 +39,11 @@ app.use("/api/users/", userRoutes);
 app.use("/api/courses/", courseRoutes);
 
 // send 404 if no other route matched
-app.use((req, res) => {
-    res.status(404).json({
-        message: "Route Not Found"
-    });
+app.use((req, res, next) => {
+    // res.status(404).json({ message: "Route Not Found" });
+    const err =new Error("Route Not Found")
+    err.status = 404 
+    next(err)
 });
 
 // setup a global error handler
